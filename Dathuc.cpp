e4fec70eca9bfabad20d2515a2092d49950a8a6a
duvntr <duvntr@gmail.com>
2026-03-29 01:37:42 +0700

    Update Dathuc.cpp

--- Dathuc.cpp
+++ Dathuc.cpp
@@ -1,67 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class DaThuc {
+class DaThuc{
 private:
     int n;
     vector<int> a;
 public:
-    DaThuc() {
-        n = 0;
-        a.resize(1, 0);
+    DaThuc(){
+        n=0;
+        a.resize(1, 0); 
     }
-    friend istream& operator>>(istream& is, DaThuc & dt) {
-        is >> dt.n;
-        dt.a.resize(dt.n + 1);
-        for(int i = 0; i <= dt.n; i++){
-            is >> dt.a[i];
+    
+    friend istream& operator>>(istream& is,DaThuc & dt){
+        is>>dt.n;
+        dt.a.resize(dt.n + 1); 
+        for(int i=0;i<=dt.n;i++){
+            is>>dt.a[i];
         }
-        return is;
+        return is; 
     }
-
-    friend ostream& operator<<(ostream& os, const DaThuc & dt) {
-        bool isZero = true;
-        for(int i = 0; i <= dt.n; i++){
-            if(dt.a[i] != 0) {
-                isZero = false;
-                if(dt.a[i] > 0 && i > 0) os << "+";
-                os << dt.a[i];
-                if (i > 0) os << "x^" << i;
+    
+    friend ostream& operator<<(ostream& os,DaThuc & dt){
+        bool first = true;
+        for(int i=0;i<=dt.n;i++){
+            if(dt.a[i] != 0){
+                if(first == false && dt.a[i] > 0){
+                    os << "+";
+                }
+                os << dt.a[i] << "x^" << i;
+                first = false;
             }
         }
-        if (isZero) os << "0";
+        if(first == true) os << "0"; 
         return os;
     }
-    friend DaThuc operator+(const DaThuc& dt1, const DaThuc& dt2){
+    
+    friend DaThuc operator+(DaThuc dt1,DaThuc dt2){
         DaThuc tong;
-        tong.n = max(dt1.n, dt2.n);
-        tong.a.resize(tong.n + 1, 0);
-        for(int i = 0; i <= tong.n; i++){
-            int heso1 = (i <= dt1.n) ? dt1.a[i] : 0;
-            int heso2 = (i <= dt2.n) ? dt2.a[i] : 0;
-            tong.a[i] = heso1 + heso2;
+        tong.n=max(dt1.n,dt2.n);
+        
+        tong.a.resize(tong.n + 1, 0); 
+        dt1.a.resize(tong.n + 1, 0);
+        dt2.a.resize(tong.n + 1, 0);
+        
+        for(int i=0;i<=tong.n;i++){
+            tong.a[i]=dt1.a[i]+dt2.a[i];
         }
         return tong;
     }
-    friend DaThuc operator-(const DaThuc& dt1, const DaThuc& dt2){
+    
+    friend DaThuc operator-(DaThuc dt1,DaThuc dt2){
         DaThuc hieu;
-        hieu.n = max(dt1.n, dt2.n);
+        hieu.n=max(dt1.n,dt2.n);
+        
         hieu.a.resize(hieu.n + 1, 0);
-        for(int i = 0; i <= hieu.n; i++){
-            int heso1 = (i <= dt1.n) ? dt1.a[i] : 0;
-            int heso2 = (i <= dt2.n) ? dt2.a[i] : 0;
-            hieu.a[i] = heso1 - heso2;
+        dt1.a.resize(hieu.n + 1, 0);
+        dt2.a.resize(hieu.n + 1, 0);
+        
+        for(int i=0;i<=hieu.n;i++){
+            hieu.a[i]=dt1.a[i]-dt2.a[i];
         }
         return hieu;
     }
-
-    friend DaThuc operator*(const DaThuc& dt1, const DaThuc& dt2){
+    
+    friend DaThuc operator*(DaThuc dt1,DaThuc dt2){
         DaThuc tich;
-        tich.n = dt1.n + dt2.n;
-        tich.a.resize(tich.n + 1, 0);
-        for(int i = 0; i <= dt1.n; i++){
-            for(int j = 0; j <= dt2.n; j++){
-                tich.a[i+j] += dt1.a[i] * dt2.a[j];
+        tich.n=dt1.n+dt2.n;
+        
+        tich.a.resize(tich.n + 1, 0); 
+        
+        for(int i=0;i<=dt1.n;i++){
+            for(int j=0;j<=dt2.n;j++){
+                tich.a[i+j]+=dt1.a[i]*dt2.a[j];
             }
         }
         return tich;
@@ -69,19 +79,22 @@ public:
 };
 
 int main(){
-    DaThuc P, Q;
+    DaThuc P,Q;
     string dau;
-    cin >> P;
-    cin >> Q;
-    cin >> dau;
-    if(dau == "+"){
-        cout << P + Q;
+    cin>>P;
+    cin>>Q;
+    cin>>dau;
+    DaThuc Tong=P+Q;
+    DaThuc Hieu=P-Q;
+    DaThuc Tich=P*Q;
+    if(dau=="+"){
+        cout<<Tong;
     }
-    else if(dau == "-"){
-        cout << P - Q;
+    else if(dau=="-"){
+        cout<<Hieu;
     }
-    else if(dau == "*"){
-        cout << P * Q;
+    else if(dau=="*"){
+        cout<<Tich;
     }
     return 0;
 }
